bb9759436bef874971e00689617b9a8490d062d6
NE7AM <ne7am@outlook.com>
2025-10-10 02:07:14 +1100

    week1

---
+++ DDES9912/Assets/DDES9912 Asset Packs/week1/Test/HelloWorld.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public int a;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello World" + a);
+
+        a = a + 2;
+    }
+}

847d847dae15fa8ed5d9d968c87812e0df52c9f8
NE7AM <ne7am@outlook.com>
2025-10-15 00:05:37 +1100

    A1_test1

---
+++ DDES9912/Assets/DDES9912 Asset Packs/Scripts/SinXBob.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SinXBob : MonoBehaviour
+{
+    public Vector3 startPosition;
+    public Vector3 sinOffset;
+    public float alpha;
+    public float sinValue;
+    public float rangeFactor;
+    public float bobSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sinValue = Mathf.Sin(alpha * Mathf.Deg2Rad);
+
+        sinOffset.x = sinValue * rangeFactor;
+
+        transform.localPosition = startPosition + sinOffset;
+
+        alpha += bobSpeed * Time.deltaTime;
+    }
+}
